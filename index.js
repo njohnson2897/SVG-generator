6467e44ae0f53b1f4af2d22d6f7ca0d93f7f2f52
Nate Johnson <njohnson2897@gmail.com>
2024-03-25 19:05:51 -0500

    adds inquirer functionality

---
+++ index.js
@@ -0,0 +1,26 @@
+const inquirer = require('Inquirer')
+
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "text",
+            message: "Please type up to 3 characters to serve as the text of your logo",
+        },
+        {
+            type: "input",
+            name: "textColor",
+            message: "What color would you like the text of your logo to be?",
+        },
+        {
+            type: "list",
+            name: "shape",
+            message: "What shape would you like your logo to be?",
+            choices: ["Triangle", "Circle", "Square"],
+        },
+        {
+            type: "input",
+            name: "shapeColor",
+            message: "What color would you like the shape to be?",
+        }
+    ]);
